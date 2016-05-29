f4eb51f19453766c4e49d52a73fa7de451b042b8
gaozhuo <1802928215@qq.com>
2016-05-29 14:43:00 +0800

    StickyLayout增加惯性滑动

--- customizeview/src/main/java/com/gaozhuo/customizeview/view/StickyLayout.java
+++ customizeview/src/main/java/com/gaozhuo/customizeview/view/StickyLayout.java
@@ -4,10 +4,13 @@ import android.content.Context;
 import android.util.AttributeSet;
 import android.util.Log;
 import android.view.MotionEvent;
+import android.view.VelocityTracker;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.OverScroller;
 
 /**
  * 布局
@@ -21,6 +24,11 @@ public class StickyLayout extends LinearLayout {
     private ListView mListView;
     private int mTopViewHeight;
     private boolean mIsTopViewHidden;
+    private int mMinFlingVelocity;
+    private int mMaxFlingVelocity;
+    private int mTouchSlop;
+    private OverScroller mScroller;
+    private VelocityTracker mVelocityTracker;
 
     public StickyLayout(Context context) {
         this(context, null);
@@ -33,6 +41,19 @@ public class StickyLayout extends LinearLayout {
 
     private void init() {
         setOrientation(LinearLayout.VERTICAL);
+        mMinFlingVelocity = ViewConfiguration.get(getContext()).getScaledMinimumFlingVelocity();
+        mMaxFlingVelocity = ViewConfiguration.get(getContext()).getScaledMaximumFlingVelocity();
+        mTouchSlop = ViewConfiguration.get(getContext()).getScaledTouchSlop();
+        mScroller = new OverScroller(getContext());
+        mVelocityTracker = VelocityTracker.obtain();
+    }
+
+    @Override
+    protected void onDetachedFromWindow() {
+        super.onDetachedFromWindow();
+        mVelocityTracker.recycle();
+        mVelocityTracker = null;
+        mScroller = null;
     }
 
     @Override
@@ -93,7 +114,7 @@ public class StickyLayout extends LinearLayout {
          * 两种情况下父控件需要事件：1，topView没有完全隐藏时；2，topView完全隐藏并且
          * ListView已滑到第一个item的顶部且继续下滑时
          */
-        if(!mIsTopViewHidden || dy > 0 && isListViewTop()){
+        if (!mIsTopViewHidden || dy > 0 && isListViewTop()) {
             return true;
         }
         return false;
@@ -102,21 +123,20 @@ public class StickyLayout extends LinearLayout {
     @Override
     public boolean onTouchEvent(MotionEvent event) {
         Log.d("gaozhuo", "onTouchEvent");
+        mVelocityTracker.addMovement(event);
         final int y = (int) event.getY();
         switch (event.getActionMasked()) {
             case MotionEvent.ACTION_DOWN:
+                if(!mScroller.isFinished()){
+                    mScroller.abortAnimation();
+                }
                 mLastY = y;
                 break;
             case MotionEvent.ACTION_MOVE:
                 int dy = y - mLastY;
                 final int scrollY = getScrollY();
 
-                /**
-                 * 滑动的范围在0到 mTopViewHeight之间，已经滑动的距离问scrollY
-                 * 所以还能滑动的距离为：0 -scrollY < -dy < mTopViewHeight - scrollY
-                 */
-                int deltaY = Math.min(Math.max(-scrollY, -dy), mTopViewHeight - scrollY);
-                scrollBy(0, deltaY);
+                scrollBy(0, -dy);
 
                 if (mIsTopViewHidden && dy < 0) {//事件交给子控件处理
                     event.setAction(MotionEvent.ACTION_DOWN);
@@ -125,6 +145,12 @@ public class StickyLayout extends LinearLayout {
 
                 break;
             case MotionEvent.ACTION_UP:
+                mVelocityTracker.computeCurrentVelocity(1000, mMaxFlingVelocity);
+                int yVelocity = (int) mVelocityTracker.getYVelocity();
+                if (Math.abs(yVelocity) > mMinFlingVelocity) {
+                    mScroller.fling(0, getScrollY(), 0, -yVelocity, 0, 0, 0, mTopViewHeight);
+                    invalidate();
+                }
                 break;
         }
         return true;
@@ -141,10 +167,19 @@ public class StickyLayout extends LinearLayout {
         return false;
     }
 
+
     @Override
-    public void scrollBy(int x, int y) {
-        super.scrollBy(x, y);
-        mIsTopViewHidden = getScrollY() == mTopViewHeight;
+    public void scrollTo(int x, int y) {
+        /**
+         * 滑动的范围在0到 mTopViewHeight之间
+         */
+        super.scrollTo(x, Math.min(Math.max(0, y), mTopViewHeight));
+
+        final int scrollY = getScrollY();
+        mIsTopViewHidden = scrollY == mTopViewHeight;
+        //设置topView透明度
+        float alpha = (mTopViewHeight - scrollY) / (float) mTopViewHeight;
+        mTopView.setAlpha(alpha);
     }
 
     @Override
@@ -167,5 +202,14 @@ public class StickyLayout extends LinearLayout {
         super.onFinishInflate();
         mTopView = getChildAt(0);
         mListView = (ListView) getChildAt(1);
+    }
+
+    @Override
+    public void computeScroll() {
+        if (mScroller.computeScrollOffset()) {
+            Log.d("gaozhuo", "computeScroll=" + mScroller.getCurrY());
+            scrollTo(0, mScroller.getCurrY());
+            invalidate();
+        }
     }
 }
