cbf6bb526e27e6ea1f3b4b11d492c078255df15f
gaozhuo <1802928215@qq.com>
2016-06-29 14:21:22 +0800

    完善SmoothImageView

--- customizeview/src/main/java/com/gaozhuo/customizeview/views/SmoothImageView.java
+++ customizeview/src/main/java/com/gaozhuo/customizeview/views/SmoothImageView.java
@@ -11,8 +11,6 @@ import android.util.Log;
 import android.widget.ImageView;
 
 /**
- *
- *
  * @author gaozhuo
  * @date 2016/5/15
  */
@@ -37,61 +35,103 @@ public class SmoothImageView extends ImageView {
         super(context, attrs, defStyleAttr);
     }
 
-    public void  setSrcRect(Rect rect){
+    public void setSrcRect(Rect rect) {
         mSrcRect = rect;
     }
 
     @Override
     protected void onDraw(Canvas canvas) {
-        if(!mIsAnimationStarting){//没有动画
+        if (!mIsAnimationStarting) {//没有动画
             super.onDraw(canvas);
             return;
         }
 
         Drawable drawable = getDrawable();
         Log.d(TAG, "drawable=" + drawable);
-        if(drawable == null){
+        if (drawable == null) {
             return;
         }
-        if(!(drawable instanceof BitmapDrawable)){
+        if (!(drawable instanceof BitmapDrawable)) {
             return;
         }
 
         Bitmap bitmap = ((BitmapDrawable) drawable).getBitmap();
-        if(bitmap == null){
+        if (bitmap == null) {
             return;
         }
 
-        calculateEndRect(bitmap);
+        if(mStartTransformParam == null){
+            calculateStartTransformParam(bitmap);
+        }
+
+        if(mEndTransformParam == null){
+            calculateEndTransformParam(bitmap);
+        }
 
     }
 
-    private void calculateEndRect(Bitmap bitmap) {
-        Rect endRect = new Rect();
+    /**
+     * 计算最终TransformParam
+     *
+     * @param bitmap
+     */
+    private void calculateEndTransformParam(Bitmap bitmap) {
         int bitmapWidth = bitmap.getWidth();
         int bitmapHeight = bitmap.getHeight();
         int imageViewWidth = getWidth();
         int imageViewHeight = getHeight();
 
         float endScale = 0;
-        if(imageViewWidth / bitmapWidth < imageViewHeight / bitmapHeight){
+        if (imageViewWidth / bitmapWidth < imageViewHeight / bitmapHeight) {
             endScale = imageViewWidth / bitmapWidth;
-        }else {
+        } else {
             endScale = imageViewHeight / bitmapHeight;
         }
-        mTransformParam = new TransformParam(endRect, endScale);
+
+        //最终展视的宽高
+        int finalWidth = (int) (bitmapWidth * endScale);
+        int finalHeight = (int) (bitmapHeight * endScale);
+
+        //图片展视在ImageView中间
+        int top = getTop() + (imageViewHeight - finalHeight) / 2;
+        int left = getLeft() + (imageViewWidth - finalWidth) / 2;
+
+        Rect endRect = new Rect(left, top, left + finalWidth, top + finalHeight);
+
+        mEndTransformParam = new TransformParam(endRect, endScale);
     }
 
-    public void startAnimationIn(){
+    /**
+     * 计算开始TransformParam
+     *
+     * @param bitmap
+     */
+    private void calculateStartTransformParam(Bitmap bitmap) {
+        int bitmapWidth = bitmap.getWidth();
+        int bitmapHeight = bitmap.getHeight();
+        int imageViewWidth = mSrcRect.width();
+        int imageViewHeight = mSrcRect.height();
+
+        float startScale = 0;
+        if (imageViewWidth / bitmapWidth < imageViewHeight / bitmapHeight) {
+            startScale = imageViewHeight / bitmapHeight;
+        } else {
+            startScale = imageViewWidth / bitmapWidth;
+        }
+        mStartTransformParam = new TransformParam(mSrcRect, startScale);
+    }
+
+
+    public void startAnimationIn() {
         mIsAnimationStarting = true;
     }
 
-    public void startAnimationOut(){
+    public void startAnimationOut() {
         mIsAnimationStarting = true;
 
     }
 
-    class TransformParam{
+    class TransformParam {
         public Rect rect;
         public float scale;
 
